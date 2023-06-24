ad2b6e02f2fe12f2b027da6eca0ddddd31de8817
Michelle Parlevliet <michelle0parlevliet@gmail.com>
2023-06-24 18:35:24 -0400

    init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+import React, { useState, useEffect } from "react";
+import "@/styles//globals.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
 }
+
+export default App
