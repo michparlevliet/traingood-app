1bbe699e3729a7d77f28cb37235972dca4ef7902
Michelle Parlevliet <michelle0parlevliet@gmail.com>
2023-08-14 19:46:51 -0400

    add next  link import

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 
 import Layout from '/components/Layout';
+import Link from 'next/link';
 
 export default function Home() {
   return (
